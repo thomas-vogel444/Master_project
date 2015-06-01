b61708c68ef909e35687fdafdaa62c20819bd347
Thomas Vogel <tomvogel01@gmail.com>
2015-06-01 18:06:11 +0100

    Major Refactoring.

--- code/dataset_generation/generate_datasets.py
+++ code/dataset_generation/generate_datasets.py
@@ -5,36 +5,25 @@ import re
 import pprint as pp
 import numpy as np
 
-# Get all the CT scan folder names
+# Canonical path names for use by the various functions
 data_directory = "../../ct_atrium"
-ct_scan_path = "../../ct_atrium/CTScan_name"
+CT_scan_path = "../../ct_atrium/CTScan_name"
 DICOM_path = "../../ct_atrium/CTScan_name/DICOMS/DICOM_name"
 
 #************************************************************
 # For each CT scan folder get all the DICOM names
 #************************************************************
-def get_dicom_names(data_directory):
-	ct_directory_pattern = re.compile("[0-9]{8}")
-	ListCTScans = [directory for directory in os.listdir(data_directory) if ct_directory_pattern.match(directory)]
+def get_DICOM_names(CT_scan_directory):
+	"""
+		Get the DICOM file names following a given pattern, i.e. a name with 8 digits in it.
+	"""
+	DICOM_directory = os.path.join(CT_scan_directory, "DICOMS")
+	DICOM_name_pattern = re.compile("^[0-9]{8}")
+	DICOM_names = []
+	for root, directory, files in os.walk(DICOM_directory):
+		DICOM_names = [myfile for myfile in files if DICOM_name_pattern.match(myfile)]
 
-	ct_scan_dictionary = {}
-	for CT_scan_name in ListCTScans:
-
-		# Get the path of a specific CT scan data directory
-		ct_scan_directory = ct_scan_path.replace("CTScan_name", CT_scan_name)
-
-		# Get the DICOM file names into a dictionary with the CT scan name as value and a list of DICOM image names as values
-		dicom_directory = os.path.join(ct_scan_directory, "DICOMS")
-		dicom_name_pattern = re.compile("^[0-9]{8}")
-		dicom_files = []
-		for root, directory, files in os.walk(dicom_directory):
-			dicom_files = [myfile for myfile in files if dicom_name_pattern.match(myfile)]
-
-		ct_scan_dictionary[CT_scan_name] = dicom_files
-	return ct_scan_dictionary
-
-ct_scan_dictionary = get_dicom_names(data_directory)
-pp.pprint(ct_scan_dictionary)
+	return DICOM_names
 
 #*****************************************************************************************
 # For a given dicom file, produce a 4D numpy array Batch Channel Width Height
@@ -44,13 +33,10 @@ pp.pprint(ct_scan_dictionary)
 #	Width   : width of the image path, i.e. 32
 #	Height  : height of the image patch, i.e. 32
 #*****************************************************************************************
-# Get the path for a given CT scan
-CT_scan_names           = ct_scan_dictionary.keys()
-CT_scan_name 			= CT_scan_names[0]
-CT_scan_dicom_filenames = ct_scan_dictionary[CT_scan_name]
-
-# Get the 3D image from the CT scan
 def get_CT_scan_array(CT_scan_name, CT_scan_dicom_filenames, dicom_height = 480, dicom_width = 480):	
+	"""
+		Get the 3D image from a given CT scan into a numpy array.
+	"""
 	number_dicoms = len(CT_scan_dicom_filenames)
 
 	# Loop through all the DICOM files for a given CT scan and get all the values into a single 3D numpy array
@@ -64,8 +50,6 @@ def get_CT_scan_array(CT_scan_name, CT_scan_dicom_filenames, dicom_height = 480,
 	    CT_scan_array[:, :, CT_scan_dicom_filenames.index(dicom_filename)] = ds.pixel_array
 
 	return CT_scan_array
-
-CT_scan_array = get_CT_scan_array(CT_scan_name, CT_scan_dicom_filenames)
 
 #*****************************************************************************************
 #									GENERATING PATCHES
@@ -206,6 +190,83 @@ def tri_planar_patch_generator(x,y,z,image_3d,patch_size):
 # nrrd_path = "../../ct_atrium/14022803/14022803.nrrd"
 
 # nrrd_data, nrrd_header = nrrd.read(nrrd_path)
+
+
+
+if __name__ == "__main__":
+	# Get the names of all the CT scans
+	ct_directory_pattern = re.compile("[0-9]{8}")
+	list_CT_scans = [directory for directory in os.listdir(data_directory) if ct_directory_pattern.match(directory)]
+
+	# For every CT scan, get a list of their associated DICOM files
+	CT_scan_dictionary = {}
+	for CT_scan in list_CT_scans:
+		CT_scan_directory = CT_scan_path.replace("CTScan_name", CT_scan)
+		CT_scan_dictionary[CT_scan] = get_DICOM_names(CT_scan_directory)
+
+
+	# For every CT scan, produce a dataset
+	f = h5py.File("mytestfile.hdf5", "w")
+	for CT_scan, DICOM_list in CT_scan_dictionary.items():
+		# Extract the 3d image into a numpy array
+		CT_scan_3d_image = get_CT_scan_array(CT_scan, DICOM_list)
+
+		# Generate 3 perpendicular patches for each data point
+		# For each voxel, produce 3 32*32 perpendicular patches with it as their centre. 
+		patch_size = 32
+		tri_planar_dataset = np.zeros((CT_scan_3d_image.size, 3, patch_size, patch_size))
+
+		dicom_height  = 480
+		dicom_width   = 480
+		number_dicoms = len(DICOM_list)
+
+		x_grid = np.arange(dicom_height)
+		y_grid = np.arange(dicom_width)
+		z_grid = np.arange(number_dicoms)
+
+		for z in z_grid:
+			print "Generating patches for the %i th dicom file..." %(z)
+			for y in y_grid:
+				for x in x_grid:
+					tri_planar_dataset[x + dicom_width*y + dicom_height*dicom_width*z, :, :, :] = tri_planar_patch_generator(x,y,z,CT_scan_3d_image,patch_size)
+
+		# Save the dataset into a HDF5 file. Each data element is saved in 1 byte of memory as it takes values between 0 and 255.
+		dataset 	 = f.create_dataset("dataset_%s"%(CT_scan), tri_planar_dataset.shape, dtype="uint8")
+		dataset[...] = tri_planar_dataset
+
+	# #********************************************************************************
+	# # Get the path for a given CT scan
+	# CT_scan_names           = CT_scan_dictionary.keys()
+	# CT_scan_name 			= CT_scan_names[0]
+	# CT_scan_dicom_filenames = CT_scan_dictionary[CT_scan_name]
+
+	# # Extract the 3D image into a numpy array
+	# CT_scan_array = get_CT_scan_array(CT_scan_name, CT_scan_dicom_filenames)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
