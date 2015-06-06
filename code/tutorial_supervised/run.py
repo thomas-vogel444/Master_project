4f4efbe0d06251c374cad4f7a9493ca7fd0f879c
Thomas Vogel <tomvogel01@gmail.com>
2015-06-06 18:07:56 +0100

    All the options can be set in the python script.

--- code/tutorial_supervised/run.py
+++ code/tutorial_supervised/run.py
@@ -1,5 +1,21 @@
 import subprocess
 
-command = "th main.lua -size reduced"
+options = {
+"seed": 1 ,
+"threads": 2,
+"size": "reduced",
+"model": "convnet",
+"maxepoch": 2,
+"save": "results",
+"learningRate": 0.1,
+"batchSize": 128,
+"weightDecay": 0.001,
+"momentum": 0.001,
+"type": "float"
+}
+
+command = "th main.lua -seed %(seed)i -threads %(threads)i -size %(size)s -model %(model)s -maxepoch %(maxepoch)i " \
+"-save %(save)s -learningRate %(learningRate)f -batchSize %(batchSize)i -weightDecay %(weightDecay)f " \
+"-momentum %(momentum)f -type %(type)s" % options
 
 subprocess.call(command, shell=True)
