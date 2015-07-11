0a8c129cc99fc02ebd67783f3d156a8f226255b4
Thomas Vogel <tomvogel01@gmail.com>
2015-07-11 13:30:33 +0100

    Minor refactoring

--- code/CNN/model.lua
+++ code/CNN/model.lua
@@ -23,20 +23,20 @@ model = nn.Sequential()
 
 -- stage 1 : mean suppresion -> filter bank -> squashing -> max pooling
 model:add(nn.SpatialConvolutionMM(nfeats, nfeaturemaps[1], filtsize, filtsize))
--- model:add(nn.ReLU())
-model:add(nn.Tanh())
+model:add(nn.ReLU())
+-- model:add(nn.Tanh())
 model:add(nn.SpatialMaxPooling(poolsize[1],poolsize[1],poolsize[1],poolsize[1]))
 -- stage 2 : mean suppresion -> filter bank -> squashing -> max pooling
 model:add(nn.SpatialConvolutionMM(nfeaturemaps[1], nfeaturemaps[2], filtsize, filtsize))
--- model:add(nn.ReLU())
-model:add(nn.Tanh())
+model:add(nn.ReLU())
+-- model:add(nn.Tanh())
 model:add(nn.SpatialMaxPooling(poolsize[2],poolsize[2],poolsize[2],poolsize[2]))
 -- stage 3 : standard 2-layer MLP:
 model:add(nn.View(nfeaturemaps[2]*featuremaps_h*featuremaps_w))
 model:add(nn.Dropout(0.5))
 model:add(nn.Linear(nfeaturemaps[2]*featuremaps_h*featuremaps_w, nfeaturemaps[3]))
--- model:add(nn.ReLU())
-model:add(nn.Tanh())
+model:add(nn.ReLU())
+-- model:add(nn.Tanh())
 model:add(nn.Dropout(0.5))
 model:add(nn.Linear(nfeaturemaps[3], noutputs))
 
