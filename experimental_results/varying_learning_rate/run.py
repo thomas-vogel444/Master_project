253194c13e6e5655c5542c021091464583dce6a2
Thomas Vogel <tomvogel01@gmail.com>
2015-07-22 06:56:24 +0100

    Minor

--- experimental_results/varying_learning_rate/run.py
+++ experimental_results/varying_learning_rate/run.py
@@ -1,38 +1,18 @@
+import subprocess
+import os
 from string import Template
 import shutil
 import subprocess
 import os
+import pprint as pp
+"""
+	Template run.py script to show you how it's done...
+"""
 
-def train_model(seed			= 1, 
-				threads			= 2, 
-				identifier 		= 1, 
-				dataset 		= "../../datasets/CNN_datasets.hdf5", 
-				size 			= "full", 
-				model 			= "convnet", 
-				maxepoch		= 15, 
-				save			= "../results", 
-				learningRate	= 1, 
-				batchSize		= 512, 
-				weightDecay		= 0.0, 
-				momentum		= 0.0, 
-				type			= "cuda"):
-
-	training_command = Template("th main.lua -seed $seed -threads $threads -identifier $identifier -dataset $dataset -size $size \
-			-model $model -maxepoch $maxepoch -save $save -learningRate $learningRate -batchSize $batchSize -weightDecay $weightDecay \
-			-momentum $momentum -type $type").substitute(
-			seed			= seed, 
-			threads			= threads, 
-			identifier		= identifier, 
-			dataset			= dataset, 
-			size			= size, 
-			model			= model, 
-			maxepoch		= maxepoch, 
-			save			= save, 
-			learningRate	= learningRate, 
-			batchSize		= batchSize, 
-			weightDecay		= weightDecay, 
-			momentum		= momentum, 
-			type			= type)
+def train_model(training_parameters):
+	training_command = "th main.lua -seed %(seed)i -threads %(threads)i -identifier %(identifier)i -dataset %(dataset)s -size %(size)s "\
+			"-model %(model)s -maxepoch %(maxepoch)i -save %(save)s -learningRate %(learningRate)f -batchSize %(batchSize)i -weightDecay %(weightDecay)f "\
+			"-momentum %(momentum)f -type %(type)s" %training_parameters
 
 	print "******************** Running the following command ********************"
 	print training_command
@@ -43,36 +23,33 @@ def train_model(seed			= 1,
 if __name__ == "__main__":
 	# Parameters for the set of experiments to be conducted
 	experiment_directory = os.getcwd()
-	code_directory = "../../code/CNN"
+	code_directory = "../code/CNN"
 	os.chdir(code_directory)
 
+	training_parameters = {
+		"seed"			: 1, 
+		"threads"		: 2, 
+		"identifier" 	: 1, 
+		"dataset" 		: os.path.abspath("../../datasets/small_CNN_datasets.hdf5"), 
+		"size" 			: "full", 
+		"model" 		: "convnet", 
+		"maxepoch"		: 1, 
+		"save"			: os.path.abspath(os.path.join(experiment_directory, "test_run")), 
+		"learningRate"	: 0.1, 
+		"batchSize"		: 512, 
+		"weightDecay"	: 0.0, 
+		"momentum"		: 0.0, 
+		"type"			: "float"
+	}
+
 	# A bunch of experiments
-	learningRates	= (10, 1, 0.1, 0.01)
-	maxepoch 		= 30
-	dataset 		= os.path.abspath("../../datasets/CNN_box_atrium_datasets.hdf5")
+	learningRates	= (0.1, 0.05, 0.01)
 
 	for identifier, learningRate in enumerate(learningRates):
-		result_directory = os.path.abspath(os.path.join(experiment_directory, str(identifier)))
-		train_model(learningRate = learningRate, identifier = identifier, save = result_directory, maxepoch = maxepoch, dataset = dataset)
+		training_parameters["identifier"] 	= identifier
+		training_parameters["save"] 		= os.path.abspath(os.path.join(experiment_directory, str(identifier)))
+		training_parameters["learningRate"] = learningRate
+
+		train_model(training_parameters)
 
 	os.chdir(experiment_directory)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
