8ab69ad0d1990e56dcc157cf5f1e195c1924a0a5
Thomas Vogel <tomvogel01@gmail.com>
2015-07-30 14:55:06 +0100

    Small refactor of the segmentation code

--- code/process_results/Segmentator.py
+++ code/process_results/Segmentator.py
@@ -8,26 +8,31 @@ class Segmentator:
 	"""
 	name_extensions = ["fixed_z", "fixed_y", "fixed_x"]
 
-	def __init__(self, model_directory, segmentation_dataset):
+	def __init__(self, segmentation_dataset):
 		self.segmentation_parameter_template = {
 				"GPU"					: 2,
 				"segmentationFile" 		: segmentation_dataset,
 				"segmentationDataset"	: "segmentation_dataset_NAME",
 				"segmentationLabels"	: "labels_NAME",
 				"segmentationValues"	: "values_NAME",
-				"predictedPath"			: os.path.join(model_directory, "predicted_labels.hdf5"),
+				"predictedPath"			: "MODEL_DIRECTORY/predicted_labels.hdf5",
 				"predictedDataset"		: "predicted_labels_NAME",
-				"modelPath"				: os.path.join(model_directory, "model.net"),
-				"imagePath"				: os.path.join(model_directory, "image_NAME.png"),
+				"modelPath"				: "MODEL_DIRECTORY/model.net",
+				"imagePath"				: "MODEL_DIRECTORY/image_NAME.png",
 				"type"					: "cuda"
 			}
 
 		if os.path.isfile(self.segmentation_parameter_template["predictedPath"]):
 			os.remove(self.segmentation_parameter_template["predictedPath"])
 
-	def segment(self):
+	def segment(self, model_directory):
+		"""
+			Segments the three segmentation datasets in the segmentation hdf5 file.
+		"""
 		for name_extension in self.name_extensions:
-			segmentation_command_options = self.get_segmentation_command_options(name_extension)
+			segmentation_parameters 	 = self.get_segmentation_parameters(model_directory)	
+			segmentation_command_options = self.get_segmentation_command_options(segmentation_parameters, name_extension)
+
 			segmentation_command = "th segment.lua -GPU %(GPU)i -segmentationFile %(segmentationFile)s -segmentationLabels %(segmentationLabels)s "\
 									"-segmentationValues %(segmentationValues)s -segmentationDataset %(segmentationDataset)s "\
 									"-predictedPath %(predictedPath)s -predictedDataset %(predictedDataset)s -imagePath %(imagePath)s "\
@@ -36,10 +41,23 @@ class Segmentator:
 			print "******************** Running the following command ********************"
 			print segmentation_command
 			print 
-			subprocess.call(segmentation_command, shell=True)
+			# subprocess.call(segmentation_command, shell=True)
 
-	def get_segmentation_command_options(self, name_extension):
-		command_options = dict(self.segmentation_parameter_template)
+	def get_segmentation_parameters(self, model_directory):
+		"""
+			Fill in the model directory in the parameter template.
+		"""
+		segmentation_parameters = dict(self.segmentation_parameter_template)
+		segmentation_parameters["predictedPath"] 	= segmentation_parameters["predictedPath"].replace("MODEL_DIRECTORY", model_directory)
+		segmentation_parameters["modelPath"] 		= segmentation_parameters["predictedPath"].replace("MODEL_DIRECTORY", model_directory)
+		segmentation_parameters["imagePath"] 		= segmentation_parameters["predictedPath"].replace("MODEL_DIRECTORY", model_directory)
+		return segmentation_parameters
+
+	def get_segmentation_command_options(self, segmentation_parameters, name_extension):
+		"""
+			Customises the command option values for each segmentation dataset.
+		"""
+		command_options = segmentation_parameters
 
 		command_options["segmentationDataset"] 	= command_options["segmentationDataset"].replace("NAME", name_extension)
 		command_options["segmentationLabels"] 	= command_options["segmentationLabels"].replace("NAME", name_extension)
--- code/process_results/run_segmentation.py
+++ code/process_results/run_segmentation.py
@@ -5,5 +5,5 @@ from Segmentator import Segmentator
 model_directory = "../../experimental_results/varying_learning_rate/0"
 segmentation_dataset_directory = "../../datasets/segmentation_datasets.hdf5"
 
-segmentator = Segmentator(model_directory, segmentation_dataset_directory)
-segmentator.segment()
+segmentator = Segmentator(segmentation_dataset_directory)
+segmentator.segment(model_directory)
