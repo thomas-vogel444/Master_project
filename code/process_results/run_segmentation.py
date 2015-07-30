caa454784b388c59321cb4ee05b67f23e46b7624
Thomas Vogel <tomvogel01@gmail.com>
2015-07-30 14:31:41 +0100

    Bug fix

--- code/process_results/run_segmentation.py
+++ code/process_results/run_segmentation.py
@@ -6,4 +6,4 @@ model_directory = "../../experimental_results/varying_learning_rate/0"
 segmentation_dataset_directory = "../../datasets/segmentation_datasets.hdf5"
 
 segmentator = Segmentator(model_directory, segmentation_dataset_directory)
-segmentator.segment(name_extension)
+segmentator.segment()
