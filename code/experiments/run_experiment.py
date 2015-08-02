f562133cde666a0ff86d8f8f8dec4e0601ff87fd
Thomas Vogel <tomvogel01@gmail.com>
2015-08-02 17:13:54 +0100

    Set up a new experiment.

--- code/experiments/run_experiment.py
+++ code/experiments/run_experiment.py
@@ -5,9 +5,9 @@ import os
 #***************************************************************************************************************
 if __name__ == "__main__":
 	# Parameters for the set of experiments to be conducted
-	experiment_name 			= "varying_learning_rate"
-	varying_parameter 			= "learningRate"
-	varying_parameter_values 	= [.5, .1, .05, .01, .005]
+	experiment_name 			= "varying_first_layer_nfeaturemaps"
+	varying_parameter 			= "nfeaturemaps"
+	varying_parameter_values 	= [[32,64,1000,1000], [64,64,1000,1000], [96,64,1000,1000], [128,64,1000,1000]]
 	segmentation_code_path		= "../CNN/segment.lua"
 	segmentation_file_path 		= "../../datasets/segmentation_datasets.hdf5"
 
