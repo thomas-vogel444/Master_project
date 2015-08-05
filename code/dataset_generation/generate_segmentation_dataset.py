1e7ffd20077a99b8b5e2708ea64b2d05fd502409
Thomas Vogel <tomvogel01@gmail.com>
2015-08-05 17:31:20 +0100

    Minor

--- code/dataset_generation/generate_segmentation_dataset.py
+++ code/dataset_generation/generate_segmentation_dataset.py
@@ -43,22 +43,22 @@ for x in range(dicom_height):
 
 print "=======> Generating the segmentation dataset for fixed y = %i from CT scan %s... <=======" %(y_slice, segmented_CT_scan.name)
 tri_planar_segmentation_dataset_fixed_y = np.zeros((segmented_CT_scan.image[:,0,:].size, 6, patch_size, patch_size))
-for z in range(number_dicoms):
-	utils.drawProgressBar(float(z)/(number_dicoms-1), 100)
-	for x in range(dicom_height):
-		tri_planar_segmentation_dataset_fixed_y[x + dicom_height*z, :, :, :] = df.generate_patches((x,y_slice,z),segmented_CT_scan.image, patch_size)
+for x in range(dicom_height):
+	utils.drawProgressBar(float(x)/(dicom_height-1), 100)
+	for z in range(number_dicoms):
+		tri_planar_segmentation_dataset_fixed_y[z + number_dicoms*x, :, :, :] = df.generate_patches((x,y_slice,z),segmented_CT_scan.image, patch_size)
 
 print "=======> Generating the segmentation dataset for fixed x = %i from CT scan %s... <=======" %(x_slice, segmented_CT_scan.name)
 tri_planar_segmentation_dataset_fixed_x = np.zeros((segmented_CT_scan.image[0,:,:].size, 6, patch_size, patch_size))
-for z in range(number_dicoms):
-	utils.drawProgressBar(float(z)/(number_dicoms-1), 100)
-	for y in range(dicom_width):
-		tri_planar_segmentation_dataset_fixed_x[y + number_dicoms*z, :, :, :] = df.generate_patches((x_slice,y,z),segmented_CT_scan.image, patch_size)
+for y in range(dicom_width):
+	utils.drawProgressBar(float(y)/(dicom_width-1), 100)
+	for z in range(number_dicoms):
+		tri_planar_segmentation_dataset_fixed_x[z + number_dicoms*y, :, :, :] = df.generate_patches((x_slice,y,z),segmented_CT_scan.image, patch_size)
 
 # ********************************************************************************************
 # 								Saving the segmentation datasets
 # ********************************************************************************************
-f 			  		  	  = h5py.File(segmentation_dataset_path, "w")
+f = h5py.File(segmentation_dataset_path, "w")
 
 print "=======> Saving the segmentation datasets for fixed z in %s <=======" %segmentation_dataset_path
 segmentation_dataset_fixed_z 				  = f.create_dataset("segmentation_dataset_fixed_z", tri_planar_segmentation_dataset_fixed_z.shape, dtype="uint32")
@@ -89,4 +89,5 @@ labels_fixed_x[...] = segmented_CT_scan.labels[x_slice,:,:]
 
 values_fixed_x 		= f.create_dataset("values_fixed_x", segmented_CT_scan.image[x_slice,:,:].shape, dtype="uint32")
 values_fixed_x[...] = segmented_CT_scan.image[x_slice,:,:]
+
 f.close()
