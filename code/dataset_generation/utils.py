cf5415e9d05823da0b0349c3066407d4746df7be
Thomas Vogel <tomvogel01@gmail.com>
2015-08-11 12:29:25 +0100

    Major refactor of the datageneration code.

--- code/dataset_generation/utils.py
+++ code/dataset_generation/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import Image
 import sys
 
 def drawProgressBar(percent, bar_length = 20):
@@ -33,3 +34,41 @@ def random_3d_indices(CT_scan_labels, n, target_label, z=None):
     return np.dstack((indices_3d[0][indices_1d], 
                                             indices_3d[1][indices_1d], 
                                             indices_3d[2][indices_1d]))[0]
+
+def padded_square_image_crop(center_coordinates,image_2d, patch_size):
+        """
+            Generate a single patch of size patch_size*patch_size.
+        """
+        x, y            = center_coordinates
+        height, width   = image_2d.shape
+        patch           = np.zeros((patch_size, patch_size))
+
+        x_min = 0
+        x_max = patch_size
+        y_min = 0
+        y_max = patch_size
+
+        if x < patch_size/2:
+            x_min = patch_size/2 - x
+
+        if x > height - patch_size/2:
+            x_max = patch_size/2 + height - x
+
+        if y < patch_size/2:
+            y_min = patch_size/2 - y
+
+        if y > width - patch_size/2:
+            y_max = patch_size/2 + width - y
+
+        patch[x_min:x_max, y_min:y_max] = image_2d[np.maximum(x-patch_size/2, 0):np.minimum(x+patch_size/2, height), 
+                             np.maximum(y-patch_size/2, 0):np.minimum(y+patch_size/2, width)]
+
+        return patch
+
+def resize_image_2d_array(image_2d_array, height, width):
+        """
+            resizes a 2d image array to have dimension height, width.
+        """
+        im = Image.fromarray(image_2d_array)
+        out = im.resize((height, width))
+        return np.array(out)
