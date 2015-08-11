ad7212589da54763fe6659045d26cd2859f098fa
eisenhower444 <tomvogel01@gmail.com>
2015-08-11 09:37:00 +0100

    Added a test result for processing

--- experimental_results/test_experiment/1/model.lua
+++ experimental_results/test_experiment/1/model.lua
@@ -9,11 +9,11 @@ patchsize   = 32
 ninputs 	= nfeats*patchsize*patchsize
 
 -- hidden units, filter sizes (for ConvNet only):
-nfeaturemaps  = { 32, 64, 1000, 1000 }
+nfeaturemaps  = { 32, 64, 1000, 500 }
 filtsize 	  = 5
-poolsize 	  = { 3, 2 }
-featuremaps_h = 2
-featuremaps_w = 2
+poolsize 	  = { 2, 2 }
+featuremaps_h = 5
+featuremaps_w = 5
 noutputs 	  = 2
 
 ----------------------------------------------------------------------
@@ -21,13 +21,13 @@ print '==> construct model'
 model = nn.Sequential()
 
 -- stage 1 : mean suppresion -> filter bank -> squashing -> max pooling
-model:add(nn.SpatialConvolutionMM(nfeats, nfeaturemaps[1], filtsize, filtsize))
-model:add(nn.ReLU())
-model:add(nn.SpatialMaxPooling(poolsize[1],poolsize[1],poolsize[1],poolsize[1]))
+model:add(cudnn.SpatialConvolution(nfeats, nfeaturemaps[1], filtsize, filtsize))
+model:add(cudnn.ReLU(true))
+model:add(cudnn.SpatialMaxPooling(poolsize[1],poolsize[1],poolsize[1],poolsize[1]))
 -- stage 2 : mean suppresion -> filter bank -> squashing -> max pooling
-model:add(nn.SpatialConvolutionMM(nfeaturemaps[1], nfeaturemaps[2], filtsize, filtsize))
-model:add(nn.ReLU())
-model:add(nn.SpatialMaxPooling(poolsize[2],poolsize[2],poolsize[2],poolsize[2]))
+model:add(cudnn.SpatialConvolution(nfeaturemaps[1], nfeaturemaps[2], filtsize, filtsize))
+model:add(cudnn.ReLU())
+model:add(cudnn.SpatialMaxPooling(poolsize[2],poolsize[2],poolsize[2],poolsize[2]))
 -- stage 3 : standard 2-layer MLP:
 model:add(nn.View(nfeaturemaps[2]*featuremaps_h*featuremaps_w))
 model:add(nn.Dropout(0.5))
@@ -38,7 +38,7 @@ model:add(nn.Linear(nfeaturemaps[3], nfeaturemaps[4]))
 model:add(nn.ReLU())
 -- model:add(nn.Tanh())
 model:add(nn.Dropout(0.5))
-model:add(nn.Linear(nfeaturemaps[3], noutputs))
+model:add(nn.Linear(nfeaturemaps[4], noutputs))
 
 model:add(nn.LogSoftMax())
 
