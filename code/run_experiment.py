3ecfb86acfc27cbb891e1be50a53ee9c56ee64a6
Thomas Vogel <tomvogel01@gmail.com>
2015-08-18 20:00:03 +0100

    Set up a experiment over a large dataset of a NN with 3 connected layers, momentum and learning rate optimised.

--- code/run_experiment.py
+++ code/run_experiment.py
@@ -23,9 +23,9 @@ if __name__ == "__main__":
 			"presavedModelPath"	: "",
 			"modelFilePath"		: os.path.join(os.path.join(NN_code_directory, "models"), model_name),
 			"maxepoch"			: 100, 
-			"learningRate"		: 0.1, 
+			"learningRate"		: 0.01, 
 			"batchSize"			: 1500*4, 
-			"momentum"			: 0.0, 
+			"momentum"			: 0.5, 
 			"training_dataset" 	: os.path.join(dataset_directory,"small_atrium_box_training_dataset_2_800_000.hdf5"),
 			"testing_dataset" 	: os.path.join(dataset_directory,"testing_dataset.hdf5")
 		}
@@ -71,46 +71,6 @@ if __name__ == "__main__":
 	dataset_directory	= os.path.join(base_project_path, "datasets")
 	model_template 		= "model_template_1_conn_layers.lua"
 
-	experiment_name 	= "varying_momentum/1_0"
+	experiment_name 	= "with_large_dataset/3_conn_layer_momentum"
 	training_parameters, model_parameters, segmentation_parameters = get_base_parameters(base_project_path, experiment_name, model_template)
-	training_parameters["momentum"] = 1
 	start_experiment(training_parameters, model_parameters, segmentation_parameters)
-
-	experiment_name 	= "varying_momentum/0_5"
-	training_parameters, model_parameters, segmentation_parameters = get_base_parameters(base_project_path, experiment_name, model_template)
-	training_parameters["momentum"] = 0.5
-	start_experiment(training_parameters, model_parameters, segmentation_parameters)
-
-	experiment_name 	= "varying_momentum/0_1"
-	training_parameters, model_parameters, segmentation_parameters = get_base_parameters(base_project_path, experiment_name, model_template)
-	training_parameters["momentum"] = 0.1
-	start_experiment(training_parameters, model_parameters, segmentation_parameters)
-
-	experiment_name 	= "varying_momentum/0_05"
-	training_parameters, model_parameters, segmentation_parameters = get_base_parameters(base_project_path, experiment_name, model_template)
-	training_parameters["momentum"] = 0.05
-	start_experiment(training_parameters, model_parameters, segmentation_parameters)
-
-	experiment_name 	= "varying_momentum/0_01"
-	training_parameters, model_parameters, segmentation_parameters = get_base_parameters(base_project_path, experiment_name, model_template)
-	training_parameters["momentum"] = 0.01
-	start_experiment(training_parameters, model_parameters, segmentation_parameters)	
-
-	experiment_name 	= "varying_momentum/0_0"
-	training_parameters, model_parameters, segmentation_parameters = get_base_parameters(base_project_path, experiment_name, model_template)
-	training_parameters["momentum"] = 0
-	start_experiment(training_parameters, model_parameters, segmentation_parameters)	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
