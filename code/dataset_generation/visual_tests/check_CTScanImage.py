723a411b3698a1453142aca156c2cc77f332bf66
Thomas Vogel <tomvogel01@gmail.com>
2015-08-21 18:03:47 +0100

    refactored the check_CTScanImage visual test

--- code/dataset_generation/visual_tests/check_CTScanImage.py
+++ code/dataset_generation/visual_tests/check_CTScanImage.py
@@ -1,10 +1,13 @@
 import sys
 sys.path.append("..")
 from CTScanImage import CTScanImage
+import utils
 import re
 import os
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import numpy as np
+
 
 data_directory = "../../../ct_atrium/testing/"
 
@@ -17,24 +20,38 @@ CT_scan_parameters_template = {
 		}
 
 CT_scan_names = [directory for directory in os.listdir(data_directory) if CT_scan_parameters_template["CT_directory_pattern"].match(directory)]
-CT_scan_name  = CT_scan_names[0]
+CT_scan_name  = CT_scan_names[1]
 
+# CT_scan_name = "14012303"
 CT_scan = CTScanImage(CT_scan_name, CT_scan_parameters_template)
 
 # Plot a bunch of cross sections from the CT scan image
-x, y, z = 250, 250, 30
+x, y, z = 200, 200, 30
+
+# Resize the saggital and coronal images to make them look better
+t_height, t_width = CT_scan.image[:,:,z].shape
+transversal_values_resized = CT_scan.image[:,:,z]
+transversal_labels_resized = CT_scan.labels[:,:,z]
+
+c_height, c_width = CT_scan.image[:,y,:].shape
+coronal_values_resized = utils.resize_image_2d_array(np.int32(CT_scan.image[:,y,:]), 3*c_width, c_height)
+coronal_labels_resized = utils.resize_image_2d_array(np.int32(CT_scan.labels[:,y,:]), 3*c_width, c_height)
+
+s_height, s_width = CT_scan.image[x,:,:].shape
+saggital_values_resized = utils.resize_image_2d_array(np.int32(CT_scan.image[x,:,:]), 3*s_width, s_height)
+saggital_labels_resized = utils.resize_image_2d_array(np.int32(CT_scan.labels[x,:,:]), 3*s_width, s_height)
 
 fig = plt.figure()
 a   = fig.add_subplot(2,3,1)
 plt.imshow(CT_scan.image[:,:,z], cmap = cm.Greys_r)
 a   = fig.add_subplot(2,3,2)
-plt.imshow(CT_scan.image[:,y,:], cmap = cm.Greys_r)
+plt.imshow(coronal_values_resized, cmap = cm.Greys_r)
 a   = fig.add_subplot(2,3,3)
-plt.imshow(CT_scan.image[x,:,:], cmap = cm.Greys_r)
+plt.imshow(saggital_values_resized, cmap = cm.Greys_r)
 a   = fig.add_subplot(2,3,4)
 plt.imshow(CT_scan.labels[:,:,z], cmap = cm.Greys_r)
 a   = fig.add_subplot(2,3,5)
-plt.imshow(CT_scan.labels[:,y,:], cmap = cm.Greys_r)
+plt.imshow(coronal_labels_resized, cmap = cm.Greys_r)
 a   = fig.add_subplot(2,3,6)
-plt.imshow(CT_scan.labels[x,:,:], cmap = cm.Greys_r)
+plt.imshow(saggital_labels_resized, cmap = cm.Greys_r)
 plt.show()
