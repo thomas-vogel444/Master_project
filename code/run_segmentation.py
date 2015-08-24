9e346ad85afa66bd4802ee4740c363d94acd548f
eisenhower444 <tomvogel01@gmail.com>
2015-08-24 21:47:31 +0100

    Added experimental results

--- code/run_segmentation.py
+++ code/run_segmentation.py
@@ -7,6 +7,10 @@ def get_slice_number(segmentation_dataset):
 	p = re.compile("segmentation_dataset_(.*).hdf5")
 	return int(p.search(segmentation_dataset).group(1))
 
+def get_CT_scan_number(segmentation_dataset):
+        p = re.compile("../datasets/segmentation_dataset_for_(.*)")
+        return int(p.search(segmentation_dataset).group(1))
+
 if __name__ == "__main__":
 	# Command line arguments
 	parser = argparse.ArgumentParser()
@@ -27,15 +31,16 @@ if __name__ == "__main__":
 	segmentation_datasets 	= [segmentation_dataset for segmentation_dataset in os.listdir(args.segmentation_dataset_directory)]
 	height, width 			= 480, 480
 
-	predicted_files_directory = os.path.join(args.model_path, "predicted_files")
+	predicted_files_directory = os.path.join(args.model_path, "predicted_files_%s"%get_CT_scan_number(args.segmentation_dataset_directory))
 	if not os.path.exists(predicted_files_directory):
 		    os.makedirs(predicted_files_directory)
 
 	# Segment all the segmentation files into predicted files
-	for segmentation_dataset in segmentation_datasets:
-		segmentation_dataset_path 	= os.path.join(args.segmentation_dataset_directory, segmentation_dataset)
-		predicted_file 				= "predicted_file_%i.hdf5"%get_slice_number(segmentation_dataset)
-		predicted_path 				= os.path.join(predicted_files_directory, predicted_file)
-		
-		print "Segmenting %s and storing the predicted results in %s"%(segmentation_dataset, predicted_file)
-		segmentator.segment(segmentation_dataset_path, predicted_path, width, height)
+	print "Segmenting files in %s"%predicted_files_directory
+#	for segmentation_dataset in segmentation_datasets:
+#		segmentation_dataset_path 	= os.path.join(args.segmentation_dataset_directory, segmentation_dataset)
+#		predicted_file 				= "predicted_file_%i.hdf5"%get_slice_number(segmentation_dataset)
+#		predicted_path 				= os.path.join(predicted_files_directory, predicted_file)
+#		
+#		print "Segmenting %s and storing the predicted results in %s"%(segmentation_dataset, predicted_file)
+#		segmentator.segment(segmentation_dataset_path, predicted_path, width, height)
