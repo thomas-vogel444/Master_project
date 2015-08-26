2c67621712a548188486bcb9c0285c9521508c1a
Thomas Vogel <tomvogel01@gmail.com>
2015-08-26 12:51:57 +0100

    Minor

--- code/seg.sh
+++ code/seg.sh
@@ -1,4 +1,4 @@
-python run_experiment.py
+# python run_experiment.py
 
 python run_segmentation.py -m ../experimental_results/varying_number_of_convolutional_layers/1_conv_layer  -s ../datasets/segmentation_dataset_for_14040204
 python run_segmentation.py -m ../experimental_results/varying_number_of_convolutional_layers/2_conv_layers -s ../datasets/segmentation_dataset_for_14040204
@@ -37,3 +37,81 @@ python run_segmentation.py -m ../experimental_results/varying_number_of_hidden_u
 # python run_segmentation.py -m ../experimental_results/varying_momentum/0_1  -s ../datasets/segmentation_dataset_for_14040204
 # python run_segmentation.py -m ../experimental_results/varying_momentum/0_5  -s ../datasets/segmentation_dataset_for_14040204
 # python run_segmentation.py -m ../experimental_results/varying_momentum/1    -s ../datasets/segmentation_dataset_for_14040204
+
+#***************************************************************************************************************************************************
+python run_segmentation.py -m ../experimental_results/varying_number_of_convolutional_layers/1_conv_layer  -s ../datasets/segmentation_dataset_for_14031201
+python run_segmentation.py -m ../experimental_results/varying_number_of_convolutional_layers/2_conv_layers -s ../datasets/segmentation_dataset_for_14031201
+python run_segmentation.py -m ../experimental_results/varying_number_of_convolutional_layers/3_conv_layers -s ../datasets/segmentation_dataset_for_14031201
+python run_segmentation.py -m ../experimental_results/varying_number_of_convolutional_layers/4_conv_layers -s ../datasets/segmentation_dataset_for_14031201
+
+python run_segmentation.py -m ../experimental_results/varying_number_of_connected_layers/1_connected_layer  -s ../datasets/segmentation_dataset_for_14031201
+python run_segmentation.py -m ../experimental_results/varying_number_of_connected_layers/2_connected_layers -s ../datasets/segmentation_dataset_for_14031201
+python run_segmentation.py -m ../experimental_results/varying_number_of_connected_layers/3_connected_layers -s ../datasets/segmentation_dataset_for_14031201
+
+python run_segmentation.py -m ../experimental_results/varying_number_of_feature_maps/starting_with_16  -s ../datasets/segmentation_dataset_for_14031201
+python run_segmentation.py -m ../experimental_results/varying_number_of_feature_maps/starting_with_32  -s ../datasets/segmentation_dataset_for_14031201
+python run_segmentation.py -m ../experimental_results/varying_number_of_feature_maps/starting_with_64  -s ../datasets/segmentation_dataset_for_14031201
+python run_segmentation.py -m ../experimental_results/varying_number_of_feature_maps/starting_with_128 -s ../datasets/segmentation_dataset_for_14031201
+
+python run_segmentation.py -m ../experimental_results/varying_number_of_hidden_units/100_hidden_units  -s ../datasets/segmentation_dataset_for_14031201
+python run_segmentation.py -m ../experimental_results/varying_number_of_hidden_units/200_hidden_units  -s ../datasets/segmentation_dataset_for_14031201
+python run_segmentation.py -m ../experimental_results/varying_number_of_hidden_units/500_hidden_units  -s ../datasets/segmentation_dataset_for_14031201
+python run_segmentation.py -m ../experimental_results/varying_number_of_hidden_units/1000_hidden_units -s ../datasets/segmentation_dataset_for_14031201
+
+# python run_segmentation.py -m ../experimental_results/varying_activation_function/ReLU    -s ../datasets/segmentation_dataset_for_14031201
+# python run_segmentation.py -m ../experimental_results/varying_activation_function/Tanh    -s ../datasets/segmentation_dataset_for_14031201
+# python run_segmentation.py -m ../experimental_results/varying_activation_function/Sigmoid -s ../datasets/segmentation_dataset_for_14031201
+
+# python run_segmentation.py -m ../experimental_results/varying_pooling_function/SpatialMaxPooling     -s ../datasets/segmentation_dataset_for_14031201
+# python run_segmentation.py -m ../experimental_results/varying_pooling_function/SpatialAveragePooling -s ../datasets/segmentation_dataset_for_14031201
+
+# python run_segmentation.py -m ../experimental_results/varying_learning_rate/0_01 -s ../datasets/segmentation_dataset_for_14031201
+# python run_segmentation.py -m ../experimental_results/varying_learning_rate/0_05 -s ../datasets/segmentation_dataset_for_14031201
+# python run_segmentation.py -m ../experimental_results/varying_learning_rate/0_1  -s ../datasets/segmentation_dataset_for_14031201
+# python run_segmentation.py -m ../experimental_results/varying_learning_rate/0_5  -s ../datasets/segmentation_dataset_for_14031201
+# python run_segmentation.py -m ../experimental_results/varying_learning_rate/1    -s ../datasets/segmentation_dataset_for_14031201
+
+# python run_segmentation.py -m ../experimental_results/varying_momentum/0    -s ../datasets/segmentation_dataset_for_14031201
+# python run_segmentation.py -m ../experimental_results/varying_momentum/0_05 -s ../datasets/segmentation_dataset_for_14031201
+# python run_segmentation.py -m ../experimental_results/varying_momentum/0_1  -s ../datasets/segmentation_dataset_for_14031201
+# python run_segmentation.py -m ../experimental_results/varying_momentum/0_5  -s ../datasets/segmentation_dataset_for_14031201
+# python run_segmentation.py -m ../experimental_results/varying_momentum/1    -s ../datasets/segmentation_dataset_for_14031201
+
+#***************************************************************************************************************************************************
+python run_segmentation.py -m ../experimental_results/varying_number_of_convolutional_layers/1_conv_layer  -s ../datasets/segmentation_dataset_for_14051404
+python run_segmentation.py -m ../experimental_results/varying_number_of_convolutional_layers/2_conv_layers -s ../datasets/segmentation_dataset_for_14051404
+python run_segmentation.py -m ../experimental_results/varying_number_of_convolutional_layers/3_conv_layers -s ../datasets/segmentation_dataset_for_14051404
+python run_segmentation.py -m ../experimental_results/varying_number_of_convolutional_layers/4_conv_layers -s ../datasets/segmentation_dataset_for_14051404
+
+python run_segmentation.py -m ../experimental_results/varying_number_of_connected_layers/1_connected_layer  -s ../datasets/segmentation_dataset_for_14051404
+python run_segmentation.py -m ../experimental_results/varying_number_of_connected_layers/2_connected_layers -s ../datasets/segmentation_dataset_for_14051404
+python run_segmentation.py -m ../experimental_results/varying_number_of_connected_layers/3_connected_layers -s ../datasets/segmentation_dataset_for_14051404
+
+python run_segmentation.py -m ../experimental_results/varying_number_of_feature_maps/starting_with_16  -s ../datasets/segmentation_dataset_for_14051404
+python run_segmentation.py -m ../experimental_results/varying_number_of_feature_maps/starting_with_32  -s ../datasets/segmentation_dataset_for_14051404
+python run_segmentation.py -m ../experimental_results/varying_number_of_feature_maps/starting_with_64  -s ../datasets/segmentation_dataset_for_14051404
+python run_segmentation.py -m ../experimental_results/varying_number_of_feature_maps/starting_with_128 -s ../datasets/segmentation_dataset_for_14051404
+
+python run_segmentation.py -m ../experimental_results/varying_number_of_hidden_units/100_hidden_units  -s ../datasets/segmentation_dataset_for_14051404
+python run_segmentation.py -m ../experimental_results/varying_number_of_hidden_units/200_hidden_units  -s ../datasets/segmentation_dataset_for_14051404
+python run_segmentation.py -m ../experimental_results/varying_number_of_hidden_units/500_hidden_units  -s ../datasets/segmentation_dataset_for_14051404
+python run_segmentation.py -m ../experimental_results/varying_number_of_hidden_units/1000_hidden_units -s ../datasets/segmentation_dataset_for_14051404
+
+# python run_segmentation.py -m ../experimental_results/varying_activation_function/ReLU    -s ../datasets/segmentation_dataset_for_14051404
+# python run_segmentation.py -m ../experimental_results/varying_activation_function/Tanh    -s ../datasets/segmentation_dataset_for_14051404
+# python run_segmentation.py -m ../experimental_results/varying_activation_function/Sigmoid -s ../datasets/segmentation_dataset_for_14051404
+
+# python run_segmentation.py -m ../experimental_results/varying_pooling_function/SpatialMaxPooling     -s ../datasets/segmentation_dataset_for_14051404
+# python run_segmentation.py -m ../experimental_results/varying_pooling_function/SpatialAveragePooling -s ../datasets/segmentation_dataset_for_14051404
+
+# python run_segmentation.py -m ../experimental_results/varying_learning_rate/0_01 -s ../datasets/segmentation_dataset_for_14051404
+# python run_segmentation.py -m ../experimental_results/varying_learning_rate/0_05 -s ../datasets/segmentation_dataset_for_14051404
+# python run_segmentation.py -m ../experimental_results/varying_learning_rate/0_1  -s ../datasets/segmentation_dataset_for_14051404
+# python run_segmentation.py -m ../experimental_results/varying_learning_rate/0_5  -s ../datasets/segmentation_dataset_for_14051404
+# python run_segmentation.py -m ../experimental_results/varying_learning_rate/1    -s ../datasets/segmentation_dataset_for_14051404
+
+# python run_segmentation.py -m ../experimental_results/varying_momentum/0    -s ../datasets/segmentation_dataset_for_14051404
+# python run_segmentation.py -m ../experimental_results/varying_momentum/0_05 -s ../datasets/segmentation_dataset_for_14051404
+# python run_segmentation.py -m ../experimental_results/varying_momentum/0_1  -s ../datasets/segmentation_dataset_for_14051404
+# python run_segmentation.py -m ../experimental_results/varying_momentum/0_5  -s ../datasets/segmentation_dataset_for_14051404
+# python run_segmentation.py -m ../experimental_results/varying_momentum/1    -s ../datasets/segmentation_dataset_for_14051404
