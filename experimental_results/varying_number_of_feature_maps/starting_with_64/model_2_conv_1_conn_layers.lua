f8ee314ff66b33d3e1fb2d8201963d1c21d46f48
eisenhower444 <tomvogel01@gmail.com>
2015-08-26 08:43:54 +0100

    Added the segmnetation results for varying number of feature maps

---
+++ experimental_results/varying_number_of_feature_maps/starting_with_64/model_2_conv_1_conn_layers.lua
@@ -0,0 +1,40 @@
+----------------------------------------------------------------------
+print '==> define parameters'
+
+-- hidden units, filter sizes (for ConvNet only):
+nfeaturemaps  = { 64, 128, 1000 }
+filtsize 	  = 5
+poolsize 	  = { 2, 2 }
+featuremaps_h = 5
+featuremaps_w = 5
+noutputs 	  = 2
+
+----------------------------------------------------------------------
+print '==> construct model'
+model = nn.Sequential()
+
+-- stage 1 : mean suppresion -> filter bank -> squashing -> max pooling
+model:add(cudnn.SpatialConvolution(nfeats, nfeaturemaps[1], filtsize, filtsize))
+model:add(cudnn.ReLU(true))
+model:add(cudnn.SpatialMaxPooling(poolsize[1],poolsize[1],poolsize[1],poolsize[1]))
+-- stage 2 : mean suppresion -> filter bank -> squashing -> max pooling
+model:add(cudnn.SpatialConvolution(nfeaturemaps[1], nfeaturemaps[2], filtsize, filtsize))
+model:add(cudnn.ReLU(true))
+model:add(cudnn.SpatialMaxPooling(poolsize[2],poolsize[2],poolsize[2],poolsize[2]))
+-- stage 2 : standard 1-layer MLP:
+model:add(nn.View(nfeaturemaps[2]*featuremaps_h*featuremaps_w))
+model:add(nn.Dropout(0.5))
+model:add(nn.Linear(nfeaturemaps[2]*featuremaps_h*featuremaps_w, nfeaturemaps[3]))
+model:add(nn.ReLU())
+model:add(nn.Dropout(0.5))
+model:add(nn.Linear(nfeaturemaps[3], noutputs))
+model:add(nn.LogSoftMax())
+
+-- 32*32 -> 28*28 -> 14*14
+
+----------------------------------------------------------------------
+print '==> here is the model:'
+print(model)
+
+----------------------------------------------------------------------
+criterion = nn.ClassNLLCriterion()
