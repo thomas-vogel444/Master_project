c21bee4aee808dd531fede876f7911adfb3f5825
Thomas Vogel <tomvogel01@gmail.com>
2015-08-28 08:11:20 +0100

    Set up a new experimeent varying sampling type

--- code/run_experiment.py
+++ code/run_experiment.py
@@ -10,11 +10,12 @@ if __name__ == "__main__":
 	# ************************************************************************************************
 	# 						Base parameters for the set of experiments to be conducted
 	# ************************************************************************************************
-	def get_base_parameters(base_project_path, experiment_name, model_template):
-		NN_code_directory 				= os.path.join(base_project_path, "code/CNN")
-		dataset_directory				= os.path.join(base_project_path, "datasets")
-		experimental_results_directory  = os.path.join(base_project_path, "experimental_results")
+	base_project_path 				= os.path.abspath("..")
+	NN_code_directory 				= os.path.join(base_project_path, "code/CNN")
+	dataset_directory				= os.path.join(base_project_path, "datasets")
+	experimental_results_directory  = os.path.join(base_project_path, "experimental_results")
 
+	def get_base_parameters(experiment_name, model_template):
 		model_name 						= model_template.replace("_template", "")
 		base_training_parameters = {
 			"GPU_identifier"	: 1,
@@ -56,43 +57,26 @@ if __name__ == "__main__":
 	# ************************************************************************************************
 	# 										Run the experiments for varying parameters
 	# ************************************************************************************************
-	base_project_path 	= os.path.abspath("..")
-	dataset_directory	= os.path.join(base_project_path, "datasets")
 	model_template 		= "model_template_2_conv_1_conn_layers.lua"
 
-	experiment_name 	= "varying_momentum/0"
-	training_parameters, model_parameters = get_base_parameters(base_project_path, experiment_name, model_template)
-	training_parameters["momentum"]  = 0
+	experiment_name 	= "varying_sampling_type/no_atrium_box"
+	training_parameters, model_parameters = get_base_parameters(experiment_name, model_template)
+	training_parameters["training_dataset"]  = os.path.join(dataset_directory,"no_atrium_box_training_dataset.hdf5")
 
 	start_experiment(training_parameters, model_parameters)
 
-	experiment_name 	= "varying_momentum/0_01"
-	training_parameters, model_parameters = get_base_parameters(base_project_path, experiment_name, model_template)
-	training_parameters["momentum"]  = 0.01
+	experiment_name 	= "varying_sampling_type/small_atrium_box"
+	training_parameters, model_parameters = get_base_parameters(experiment_name, model_template)
+	training_parameters["training_dataset"]  = os.path.join(dataset_directory,"small_atrium_box_training_dataset.hdf5")
 
 	start_experiment(training_parameters, model_parameters)
 
-	experiment_name 	= "varying_momentum/0_05"
-	training_parameters, model_parameters = get_base_parameters(base_project_path, experiment_name, model_template)
-	training_parameters["momentum"]  = 0.05
+	experiment_name 	= "varying_sampling_type/large_atrium_box"
+	training_parameters, model_parameters = get_base_parameters(experiment_name, model_template)
+	training_parameters["training_dataset"]  = os.path.join(dataset_directory,"large_atrium_box_training_dataset.hdf5")
 
 	start_experiment(training_parameters, model_parameters)
 
-	experiment_name 	= "varying_momentum/0_1"
-	training_parameters, model_parameters = get_base_parameters(base_project_path, experiment_name, model_template)
-	training_parameters["momentum"]  = 0.1
 
-	start_experiment(training_parameters, model_parameters)
 
-	experiment_name 	= "varying_momentum/0_5"
-	training_parameters, model_parameters = get_base_parameters(base_project_path, experiment_name, model_template)
-	training_parameters["momentum"]  = 0.5
-
-	start_experiment(training_parameters, model_parameters)
-
-	experiment_name 	= "varying_momentum/1"
-	training_parameters, model_parameters = get_base_parameters(base_project_path, experiment_name, model_template)
-	training_parameters["momentum"]  = 1
-
-	start_experiment(training_parameters, model_parameters)
 
