cd6dbdbde2b7aea8fe5a033447c368fd9989cdca
eisenhower444 <tomvogel01@gmail.com>
2015-08-29 11:42:01 +0100

    Added experimental results

--- experimental_results/varying_learning_rate/0_5/model_2_conv_1_conn_layers.lua
+++ experimental_results/varying_learning_rate/0_5/model_2_conv_1_conn_layers.lua
@@ -2,7 +2,7 @@
 print '==> define parameters'
 
 -- hidden units, filter sizes (for ConvNet only):
-nfeaturemaps  = { 16, 32, 500 }
+nfeaturemaps  = { 32, 64, 100 }
 filtsize 	  = 5
 poolsize 	  = { 2, 2 }
 featuremaps_h = 5
@@ -16,11 +16,11 @@ model = nn.Sequential()
 -- stage 1 : mean suppresion -> filter bank -> squashing -> max pooling
 model:add(cudnn.SpatialConvolution(nfeats, nfeaturemaps[1], filtsize, filtsize))
 model:add(cudnn.ReLU(true))
-model:add(cudnn.SpatialAveragePooling(poolsize[1],poolsize[1],poolsize[1],poolsize[1]))
+model:add(cudnn.SpatialMaxPooling(poolsize[1],poolsize[1],poolsize[1],poolsize[1]))
 -- stage 2 : mean suppresion -> filter bank -> squashing -> max pooling
 model:add(cudnn.SpatialConvolution(nfeaturemaps[1], nfeaturemaps[2], filtsize, filtsize))
 model:add(cudnn.ReLU(true))
-model:add(cudnn.SpatialAveragePooling(poolsize[2],poolsize[2],poolsize[2],poolsize[2]))
+model:add(cudnn.SpatialMaxPooling(poolsize[2],poolsize[2],poolsize[2],poolsize[2]))
 -- stage 2 : standard 1-layer MLP:
 model:add(nn.View(nfeaturemaps[2]*featuremaps_h*featuremaps_w))
 model:add(nn.Dropout(0.5))
