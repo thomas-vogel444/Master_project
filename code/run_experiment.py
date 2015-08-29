2aeecaed8826b8f56be54e3a94f26b4d450b9d26
Thomas Vogel <tomvogel01@gmail.com>
2015-08-29 22:42:34 +0100

    Added a new experiment

--- code/run_experiment.py
+++ code/run_experiment.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
 			"maxepoch"			: 100, 
 			"learningRate"		: 0.5, 
 			"batchSize"			: 1500*4, 
-			"momentum"			: 0, 
+			"momentum"			: 0.5, 
 			"training_dataset" 	: os.path.join(dataset_directory,"no_atrium_box_training_dataset.hdf5"),
 			"testing_dataset" 	: os.path.join(dataset_directory,"testing_dataset.hdf5")
 		}
@@ -59,26 +59,20 @@ if __name__ == "__main__":
 	# ************************************************************************************************
 	model_template 		= "model_template_2_conv_1_conn_layers.lua"
 
-	experiment_name 	= "varying_momentum/0"
+	experiment_name 	= "varying_training_dataset/small_atrium_box"
 	training_parameters, model_parameters = get_base_parameters(experiment_name, model_template)
-	training_parameters["momentum"]  = 0
+	training_parameters["training_dataset"]  = "small_atrium_box_training_dataset.hdf5"
 
 	start_experiment(training_parameters, model_parameters)
 
-	experiment_name 	= "varying_momentum/0_05"
+	experiment_name 	= "varying_training_dataset/large_atrium_box"
 	training_parameters, model_parameters = get_base_parameters(experiment_name, model_template)
-	training_parameters["momentum"]  = 0.05
+	training_parameters["training_dataset"]  = "large_atrium_box_training_dataset.hdf5"
 
 	start_experiment(training_parameters, model_parameters)
 
-	experiment_name 	= "varying_momentum/0_1"
+	experiment_name 	= "varying_training_dataset/no_atrium_box"
 	training_parameters, model_parameters = get_base_parameters(experiment_name, model_template)
-	training_parameters["momentum"]  = 0.1
-
-	start_experiment(training_parameters, model_parameters)
-
-	experiment_name 	= "varying_momentum/0_5"
-	training_parameters, model_parameters = get_base_parameters(experiment_name, model_template)
-	training_parameters["momentum"]  = 0.5
+	training_parameters["training_dataset"]  = "no_atrium_box_training_dataset.hdf5"
 
 	start_experiment(training_parameters, model_parameters)
