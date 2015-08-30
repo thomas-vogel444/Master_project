1c9962b93550890edca94f61f6b4a3d7e851ef53
Thomas Vogel <tomvogel01@gmail.com>
2015-08-30 06:21:59 +0100

    Bug Fix

--- code/run_experiment.py
+++ code/run_experiment.py
@@ -61,18 +61,18 @@ if __name__ == "__main__":
 
 	experiment_name 	= "varying_training_dataset/small_atrium_box"
 	training_parameters, model_parameters = get_base_parameters(experiment_name, model_template)
-	training_parameters["training_dataset"]  = "small_atrium_box_training_dataset.hdf5"
+	training_parameters["training_dataset"]  = os.path.join(dataset_directory, "small_atrium_box_training_dataset.hdf5")
 
 	start_experiment(training_parameters, model_parameters)
 
 	experiment_name 	= "varying_training_dataset/large_atrium_box"
 	training_parameters, model_parameters = get_base_parameters(experiment_name, model_template)
-	training_parameters["training_dataset"]  = "large_atrium_box_training_dataset.hdf5"
+	training_parameters["training_dataset"]  = os.path.join(dataset_directory, "large_atrium_box_training_dataset.hdf5")
 
 	start_experiment(training_parameters, model_parameters)
 
 	experiment_name 	= "varying_training_dataset/no_atrium_box"
 	training_parameters, model_parameters = get_base_parameters(experiment_name, model_template)
-	training_parameters["training_dataset"]  = "no_atrium_box_training_dataset.hdf5"
+	training_parameters["training_dataset"]  = os.path.join(dataset_directory, "no_atrium_box_training_dataset.hdf5")
 
 	start_experiment(training_parameters, model_parameters)
